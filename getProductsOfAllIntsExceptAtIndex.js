79329abbdeaa15cd15a4416c9473e2570bdc3f5c
Jesse Beard <61173820+jessebeard@users.noreply.github.com>
2021-02-08 13:59:47 -0800

    wrote out pseudocode

--- getProductsOfAllIntsExceptAtIndex.js
+++ getProductsOfAllIntsExceptAtIndex.js
@@ -1,1 +1,8 @@
-getProductsOfAllIntsExceptAtIndex
+const getProductsOfAllIntsExceptAtIndex = ( intArray ) => {
+  //create an array with the cumulative values of the right side of the array.
+  //populate the array
+  //pop first value of that array to result array
+  //create running total of left side
+  //iterate through both arrays, multiplying the running total with the next value in the right hand array,
+  // then adjust the running total
+}
