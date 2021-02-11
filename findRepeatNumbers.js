47363e31a97c0654e9eaf23497edd3a6cbbdb519
Jesse Beard <61173820+jessebeard@users.noreply.github.com>
2021-02-10 23:57:30 -0800

    looked into my old work on this one and startd out a plan

--- findRepeatNumbers.js
+++ findRepeatNumbers.js
@@ -1,13 +1,29 @@
 function findRepeat(numbers) {
-  for ( let i = 0, j = 0, k = 0; i < numbers.length; i += 1) {
-    if (numbers[j] === numbers[numbers[k]]) {
-       return numbers[j]
-    } else {
-      j = numbers[j];
-      k = numbers[numbers[k]]
-    }
-  }
-  return false;
+  // we are looking at a special case of an array, which contains
+  // values 1..n and has a length of n + 1. As such it must contain at
+  // least one reapeat. We aim to find a repeated number with the key constraint
+  // being O1 MEMORY.
+  //the idea is to try and traverse the array in a torise hare style in
+  // order to find the duplicate
+  //
+  //Tortoise and Hare
+  //* for ( let i = 0, j = 0, k = 0; i < numbers.length * 2 ; i += 1) {
+  //*   if (numbers[j] === numbers[numbers[k]]){
+  //*      return numbers[k]
+  //*   } else {
+  //*     j = numbers[j];
+  //*     k = numbers[numbers[k]]
+  //*   }
+  //* }
+  //* return false;
+  // this actually warks for the first two tests, if only by coincidence. But it
+  // does teach us a lesson we can also reason -- a repeat will exist in a "loop"
+  // of the linked list
+  //...
+  // because of the properties of the array, we can reason that the last index
+  // resembles the head of a linked list, it will never have a pointer to it,
+  // once we are in the linked list, we no that its start will be a repeat.
+
 }
 
 // Tests
@@ -15,7 +31,7 @@ function findRepeat(numbers) {
 let desc = 'just the repeated number';
 let actual = findRepeat([1, 1]);
 let expected = 1;
-assertEqual(actual, expected, desc);
+assertEqual(actual, expected, desc)
 
 desc = 'short array';
 actual = findRepeat([1, 2, 3, 2]);
@@ -23,12 +39,12 @@ expected = 2;
 assertEqual(actual, expected, desc);
 
 desc = 'medium array';
-actual = findRepeat([1, 2, 5, 5, 5, 5]);
+actual = findRepeat([1, 5, 2, 3, 4, 5]);
 expected = 5;
 assertEqual(actual, expected, desc);
 
 desc = 'long array';
-actual = findRepeat([4, 1, 4, 8, 3, 2, 7, 6, 5]);
+actual = findRepeat([4, 1, 4, 8, 4, 3, 7, 6, 5]);
 expected = 4;
 assertEqual(actual, expected, desc);
 
@@ -36,6 +52,6 @@ function assertEqual(a, b, desc) {
   if (a === b) {
     console.log(`${desc} ... PASS`);
   } else {
-    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+    console.log(`no ${desc} ... FAIL: ${a} != ${b}`);
   }
 }
