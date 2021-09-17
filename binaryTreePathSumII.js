bb0a116eb69e25888c37e41c591362e56f0756c2
jessebeard <61173820+jessebeard@users.noreply.github.com>
2021-09-16 23:18:24 -0700

    added an updated version that has better memory consumption.

--- binaryTreePathSumII.js
+++ binaryTreePathSumII.js
@@ -11,6 +11,7 @@
  * @param {number} targetSum
  * @return {number[][]}
  */
+// original
  var pathSum = function(root, targetSum) {
     // result will be an array
   let result = [];
@@ -38,6 +39,57 @@
       if(node.right){
         recursivePathAdder(node.right, path.map(x=>x), target-node.val)
       }
+    }
+  })(root, new Array, targetSum)
+  // return result array 
+  return result;
+};
+
+
+// a much less memory intensive way of doing this is to push 
+// the value in and pop it off after the recursion.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+ var pathSum = function(root, targetSum) {
+    // result will be an array
+  let result = [];
+    // recurisve function which takes a node, a path array, a target
+  if (!root) return result;
+  (function recursivePathAdder(node, path, target) {
+    // if node is leaf
+    if(!node.right && !node.left){
+      // if target - node value is 0 add value to path array
+      if(target - node.val === 0){
+      // create duplicate array for result and add leaf value
+        result.push([...path, node.val])
+      }
+    // else node is not a leaf 
+    } else {
+      // push value to path array
+      path.push(node.val)
+      // if has left child 
+      if(node.left){
+        // recurse on left child with target - path
+        recursivePathAdder(node.left, path, target-node.val)
+      }
+      // same for right child
+      if(node.right){
+        recursivePathAdder(node.right, path, target-node.val)
+      }
+      //pop off node.val before going up the tree
+      path.pop();
     }
   })(root, new Array, targetSum)
   // return result array 
