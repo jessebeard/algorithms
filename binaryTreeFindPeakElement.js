1582ee6c0120ea94f627ba5db6df67f52d766029
jessebeard <61173820+jessebeard@users.noreply.github.com>
2021-09-24 17:01:23 -0700

    passing Submission

---
+++ binaryTreeFindPeakElement.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length -1;
+    while (left + 1 < right) {
+      let mid = ~~((left + right) / 2);
+      let isPeak = nums[mid-1] < nums[mid] && nums[mid] > nums[mid + 1];
+      if (isPeak) return mid;
+      if (nums[mid] < nums[mid - 1]) right = mid;
+      else left = mid;
+    }
+    if (nums[left] > nums[right]) return left;
+    return right;
+  };
