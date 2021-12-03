05a9de58ed957f2d3974f72991e8108c14f96a2e
Jesse Beard <61173820+jessebeard@users.noreply.github.com>
2021-12-03 00:50:03 -0800

    finished part 2

--- advent of code 2021/day3.js
+++ advent of code 2021/day3.js
@@ -18,3 +18,43 @@ const gammaRate = parseInt(gammaRateBinary.join(""), 2)
 const epsilonRate = parseInt(epsilonRateBinary.join(""), 2)
 
 console.log(gammaRate, epsilonRate, epsilonRate*gammaRate)
+
+// part 2
+const text = document.querySelector('pre').innerText;
+const binaryStringsArray = text.trim().split("\n");
+const arrayLength = binaryStringsArray[0].length;
+
+const binaryArrayList = binaryStringsArray.map(x => x.split(""))
+  .map(binaryArray => binaryArray.map(char => parseInt(char)));
+
+let index = 0
+let o2binary = [...binaryArrayList]
+while (o2binary.length > 1) {
+  const length = o2binary.length;
+  const currentMost = o2binary
+    .reduce((acc, cur) => acc += cur[index], 0);
+	console.log(currentMost)
+  const filterTarget = currentMost >= length/2 ? 1 : 0;
+  o2binary = o2binary
+		.filter(num => num[index] === filterTarget);
+  index += 1;
+}
+console.log(o2binary)
+const o2value = parseInt(o2binary[0].join(''), 2)
+
+index = 0
+let co2scrubBinary = [...binaryArrayList]
+while (co2scrubBinary.length > 1) {
+  const length = co2scrubBinary.length;
+  const currentMost = co2scrubBinary
+    .reduce((acc, cur) => acc += cur[index], 0);
+  const filterTarget = currentMost >= length/2 ? 0 : 1;
+  co2scrubBinary = co2scrubBinary
+		.filter(num => num[index] === filterTarget);
+	console.log(co2scrubBinary)
+  index += 1;
+}
+console.log(co2scrubBinary)
+const co2scrubValue = parseInt(co2scrubBinary[0].join(''), 2)
+
+console.log(o2value, co2scrubValue, o2value * co2scrubValue)
