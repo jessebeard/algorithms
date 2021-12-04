d6257b7c8bb875c3548e4ea57f24d53c1af10dd5
Jesse Beard <61173820+jessebeard@users.noreply.github.com>
2021-12-03 22:13:05 -0800

    part 2

--- advent of code 2021/day4.js
+++ advent of code 2021/day4.js
@@ -7,10 +7,10 @@ textArr.shift()
 const boardsArr = []
 let currentBoard = []
 for (let i = 1; i < textArr.length; i++){
- 	currentBoard.push(textArr[i-1].trim()
+   currentBoard.push(textArr[i-1].trim()
     .split(/\s+/).map(n => parseInt(n)))
-	if(i % 5 === 0){
-		boardsArr.push(currentBoard)
+  if(i % 5 === 0){
+    boardsArr.push(currentBoard)
     currentBoard = []
     textArr.shift()
   }
@@ -18,11 +18,11 @@ for (let i = 1; i < textArr.length; i++){
 let winnerIndex = 0
 const checkRows = (board) => {
   for(let i = 0; i < board.length; i++){
-	  for(let y = 0; y < 5; y++){
+    for(let y = 0; y < 5; y++){
       let current = 0
       for(let x = 0; x < 5; x++){
-    		current += board[i][y][x]
-    	}
+        current += board[i][y][x]
+      }
       if (current == -5){
         winnerIndex = i
         return true
@@ -33,11 +33,11 @@ const checkRows = (board) => {
 }
 const checkCols = (board) => {
   for(let i = 0; i < board.length; i++){
-	  for(let x = 0; x < 5; x++){
+    for(let x = 0; x < 5; x++){
       let current = 0
       for(let y = 0; y < 5; y++){
-    		current += board[i][y][x]
-    	}
+        current += board[i][y][x]
+      }
       if (current == -5){
         winnerIndex = i
         return true
@@ -59,24 +59,105 @@ while (keepRolling) {
   index++
   curRoll = nums[index]
   if(checkCols(cards) || checkRows(cards)){
-		keepRolling = false
+    keepRolling = false
   }
   if(index>1000) keepRolling = false
 }
 
 curRoll = nums[index-1]
 
-		const bingoBoard = cards[winnerIndex]
-		console.log(bingoBoard, winnerIndex)
-		let total = 0
-	  for(let x = 0; x < 5; x++){
-      for(let y = 0; y < 5; y++){
-        if (bingoBoard[y][x] !== -1){
-    		  total += bingoBoard[y][x]
-      	}
-      }
+const bingoBoard = cards[winnerIndex]
+console.log(bingoBoard, winnerIndex)
+let total = 0
+for(let x = 0; x < 5; x++){
+  for(let y = 0; y < 5; y++){
+    if (bingoBoard[y][x] !== -1){
+      total += bingoBoard[y][x]
     }
-
+  }
+}
 
 console.log(total, curRoll, total*curRoll)
 
+//part 2
+const text = document.querySelector('pre').innerText;
+let textArr = text.trim().split("\n");
+const nums =  textArr.shift().split(',').map(x=>parseInt(x))
+textArr.shift()
+const boardsArr = []
+let currentBoard = []
+for (let i = 1; i < textArr.length; i++){
+   currentBoard.push(textArr[i-1].trim()
+    .split(/\s+/).map(n => parseInt(n)))
+  if(i % 5 === 0){
+    boardsArr.push(currentBoard)
+    currentBoard = []
+    textArr.shift()
+  }
+}
+const winners = new Set()
+let winnerIndex = []
+const checkRows = (board) => {
+  for(let i = 0; i < board.length; i++){
+    for(let y = 0; y < 5; y++){
+      let current = 0
+      for(let x = 0; x < 5; x++){
+        current += board[i][y][x]
+      }
+      if (current == -5){
+        if(!winners.has(i)) {
+          winners.add(i)
+          winnerIndex.push(i)
+        }
+      }
+    }
+  }
+}
+const checkCols = (board) => {
+  for(let i = 0; i < board.length; i++){
+    for(let x = 0; x < 5; x++){
+      let current = 0
+      for(let y = 0; y < 5; y++){
+        current += board[i][y][x]
+      }
+      if (current == -5){
+         if(!winners.has(i)){
+          winners.add(i)
+          winnerIndex.push(i)
+        }
+      }
+    }
+  }
+}
+let cards = boardsArr
+let keepRolling = true
+let index = 0
+let curRoll = nums[index]
+while (winnerIndex.length < boardsArr.length) {
+  cards = cards.map(card=> (
+    card.map(row => (
+      row.map(num => num === curRoll ? -1: num)
+    ))
+  ))
+  index++
+  curRoll = nums[index]
+  if(checkCols(cards) || checkRows(cards)){
+    keepRolling = false
+  }
+  if(index>1000) keepRolling = false
+}
+
+curRoll = nums[index-1]
+
+const bingoBoard = cards[winnerIndex[winnerIndex.length - 1]]
+console.log(bingoBoard, winnerIndex[winnerIndex.length - 1])
+let total = 0
+for(let x = 0; x < 5; x++){
+  for(let y = 0; y < 5; y++){
+    if (bingoBoard[y][x] !== -1){
+      total += bingoBoard[y][x]
+    }
+  }
+}
+
+console.log(total, curRoll, total*curRoll)
