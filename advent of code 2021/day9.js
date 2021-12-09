b686dec06eb8c61cdb5f21b811a8bebacf5ee43d
Jesse Beard <61173820+jessebeard@users.noreply.github.com>
2021-12-08 23:50:24 -0800

    🧹housekeeping 🧼

--- advent of code 2021/day9.js
+++ advent of code 2021/day9.js
@@ -3,6 +3,24 @@
   const dM = lines.map(x => x.split("").map(x=> parseInt(x)))
   const dirs = [[1,0],[0,1],[-1,0],[0,-1]]
   const lowPoints = []
+  const basinSizes = []
+	const checked = new Set()
+  const findBasinSize = (x,y,set) => {
+    let count = 1
+    dirs.forEach(([dx,dy])=> {
+      const nY = dy+y
+      const nX = dx+x
+      if ( nX>=0 && nX!==dM[0].length
+      && nY>=0 && nY<dM.length
+      && dM[nY][nX] < 9 && dM[nY][nX] > dM[y][x]
+			&& !set.has(`${nX},${nY}`)) {
+        set.add(`${nX},${nY}`)
+        count += findBasinSize(nX, nY, set)
+      }
+    })
+    return count;
+  }
+
   dM.forEach((line, y)=> line.forEach((loc, x)=> {
     const isLow = dirs.reduce((a, [dx,dy])=> {
       if (a && dx+x>=0 && dx+x!==line.length
@@ -13,30 +31,9 @@
     }, true)
     if (isLow){
       lowPoints.push([x,y])
+      basinSizes.push(findBasinSize(x,y,checked,0))
     }
   }))
-  const findBasinSize = (x,y,set) => {
-    let count = 1
-    dirs.forEach(([dx,dy])=>{
-      const nY = dy+y
-      const nX = dx+x
-      if (!set.has(`${nX},${nY}`)
-      && nX>=0 && nX!==dM[0].length
-      && nY>=0 && nY<dM.length
-      && dM[nY][nX] < 9 && dM[nY][nX] > dM[y][x]) {
-        set.add(`${nX},${nY}`)
-        count += findBasinSize(nX, nY, set)
-      }
-    })
-    return count;
-  }
-  const basinSizes = []
-  lowPoints.forEach(([x,y])=> {
-		const checked = new Set()
-    let basinSize = findBasinSize(x,y,checked,0)
-    basinSizes.push(basinSize)
-  })
-
   console.log(`there are`)
   console.log(lowPoints.reduce((a,[x,y]) => dM[y][x]+1+a, 0))
   console.log(`danger zones, and the product of the area of the 3 biggest is`)
